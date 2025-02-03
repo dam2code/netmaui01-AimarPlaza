870a1f582f8e0775dfe8d9d5a1372d5bad62d9b3
Aimar Plaza <olga.aimar.ch@gmail.com>
2025-02-03 19:17:56 +0100

    Trabajo 1 Finalizado

---
+++ Phoneword/Class2.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Phoneword
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp() =>
+            MauiApp.CreateBuilder()
+                   .UseMauiApp<App>()
+                   .Build();
+    }
+}
