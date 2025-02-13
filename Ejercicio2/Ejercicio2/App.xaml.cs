56c92ec19cae2c1697b86cd56477e427a30bc35c
Aimar Plaza <olga.aimar.ch@gmail.com>
2025-02-13 16:19:08 +0100

    Trabajo 2

---
+++ Ejercicio2/Ejercicio2/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejercicio2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+    }
+}
---
+++ Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Notes // En lugar de Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+        public const double MyFontSize = 28;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+            DisplayAlert("Success", "Note saved!", "OK");
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+                editor.Text = string.Empty;
+                DisplayAlert("Success", "Note deleted!", "OK");
+            }
+        }
+    }
+}
