ca7f591fc7394a42652b92ce9232043df8965837
Aimar Plaza <olga.aimar.ch@gmail.com>
2025-02-15 17:29:29 +0100

    Ejercicio 5 de 3

--- Astronomy/Astronomy/AppShell.xaml.cs
+++ Astronomy/Astronomy/AppShell.xaml.cs
@@ -5,5 +5,28 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
+    }
+    public AstronomicalBodiesPage()
+    {
+        InitializeComponent();
+
+        btnComet.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=comet");
+        btnEarth.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=earth");
+        btnMoon.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=moon");
+        btnSun.Clicked += async (s, e) => await Shell.Current.GoToAsync("astronomicalbodydetails?astroName=sun");
+    }
+    string astroName;
+    public string AstroName
+    {
+        get => astroName;
+        set
+        {
+            astroName = value;
+
+            // this is a custom function to update the UI immediately
+            UpdateAstroBodyUI(astroName);
+        }
     }
 }
